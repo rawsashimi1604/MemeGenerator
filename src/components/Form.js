bb5ee34c31ff63c7ca63aff23700b4dbb8232735
Gavin Loo <looweiren@gmail.com>
2022-04-28 19:05:40 +0800

    added form logic to handle onChange events on inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -18,6 +18,18 @@ export default function Form() {
         randomImage: "https://i.imgflip.com/c2qn.jpg",
     })
 
+    function handleChange(event) {
+
+        const {name, value} = event.target
+
+        setMemeState(prevFormState => {
+            return {
+                ...memeState,
+                [name]: value
+            }
+        })
+    }
+
 
     function getRandomMemeData() {
         let arrLen = allMemeImages.data.memes.length;
@@ -33,13 +45,21 @@ export default function Form() {
 
     return (
         <main>
-            <form className="form">
+            <form 
+                className="form"
+            >
                 <div className="form-inputs">
                     <input
                         placeholder="top input"
+                        type="text"
+                        name="topText"
+                        onChange={handleChange}
                     ></input>
                     <input
                         placeholder="bottom input"
+                        type="text"
+                        name="bottomText"
+                        onChange={handleChange}
                     ></input>
                 </div>
                 
@@ -49,8 +69,8 @@ export default function Form() {
             </form>
             <section className="meme">
                 <img src={memeState.randomImage}></img>
-                <h2 className="meme-text top">One does not simply</h2>
-                <h2 className="meme-text bottom">Walk into Mordor</h2>
+                <h2 className="meme-text top">{memeState.topText}</h2>
+                <h2 className="meme-text bottom">{memeState.bottomText}</h2>
             </section>
         </main>
         
